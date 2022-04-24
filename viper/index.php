426835c18b2b493cba04a03759611ba7ab3c5443
abraranik-sudo <61088935+abraranik-sudo@users.noreply.github.com>
2022-04-24 15:20:02 +0600

    Add files via upload
    learned php syntax, first code, and error and how to fix'em

---
+++ viper/index.php
@@ -0,0 +1,23 @@
+<?php 
+echo "My First php code";
+echo "My First php code error test";
+echo "My First php code check";
+
+
+echo "<h2>My First php code error test</h2>";
+
+
+
+?>
+
+
+<h1>
+<?php 
+
+
+echo "My First php code error test";
+
+
+
+?>
+</h1>
