57afe50222b7c731fcb3ca5c1e873b1ac45d04ee
abraranik-sudo <61088935+abraranik-sudo@users.noreply.github.com>
2022-04-27 16:58:13 +0600

    Add files via upload
    learned today php variables, single and multiple comments, arithmatic operations

---
+++ day 2.php
@@ -0,0 +1,29 @@
+<?php 
+$anik = "hello world";
+echo $anik. " amar khida lagse";
+
+$first_name = "amar khida lagse";
+$last_name = "why i'm so hungry.bro!";
+echo "<br> ". $first_name . " " . $last_name;
+
+
+
+$first_name = "amar khida lagse";
+$last_name = "why i'm so hungry.bro!";
+echo "<br> ". $first_name . " " . "&nbsp;&nbsp;&nbsp" . $last_name;
+
+
+//single line comment
+
+echo "<br> ". "//" ."..";
+
+
+/*
+multi
+line 
+comment
+*/
+
+
+
+?>
